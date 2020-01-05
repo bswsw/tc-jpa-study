70e4a8ef5626bb19763f5599f2067b5a5c64f6c5
baegoony <baegoony@gmail.com>
2020-01-05 22:49:08 +0900

    초기 세팅

---
+++ README.md
@@ -0,0 +1,8 @@
+# JPA 스터디 레포
+
+## 준비하기
+1. develop 브랜치 클론
+2. 도커 mysql 설치
+    ```
+   docker run --name mysql -e MYSQL_ROOT_PASSWORD=password -d mysql:5.7.28 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+   ```
---
+++ src/main/kotlin/com/baegoon/jpastudy/JpaStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.baegoon.jpastudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JpaStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<JpaStudyApplication>(*args)
+}
