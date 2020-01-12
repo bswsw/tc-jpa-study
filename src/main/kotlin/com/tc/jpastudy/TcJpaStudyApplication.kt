0920d0832bc8a08b5d14530239da99413845095f
baegoony <baegoony@gmail.com>
2020-01-12 21:46:50 +0900

    finish

--- src/main/kotlin/com/tc/jpastudy/TcJpaStudyApplication.kt
+++ src/main/kotlin/com/tc/jpastudy/TcJpaStudyApplication.kt
@@ -2,55 +2,36 @@ package com.tc.jpastudy
 
 import com.tc.jpastudy.domain.Car
 import com.tc.jpastudy.domain.Zone
-import com.tc.jpastudy.domain.inheritance.join.Book
-import com.tc.jpastudy.domain.inheritance.single.Image
-import com.tc.jpastudy.domain.inheritance.single.Zip
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.transaction.annotation.Transactional
 import java.time.LocalTime
-import javax.persistence.EntityManagerFactory
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
 
 @SpringBootApplication
-class TcJpaStudyApplication(
-    private val emf: EntityManagerFactory
-): ApplicationRunner {
+@Transactional
+class TcJpaStudyApplication : ApplicationRunner {
+
+    @PersistenceContext
+    private lateinit var em: EntityManager
 
     override fun run(args: ApplicationArguments?) {
-        val em = emf.createEntityManager()
-        val tx = em.transaction
+        val zone = Zone(
+            name = "주차장",
+            address = "서울시",
+            openTime = LocalTime.now(),
+            closeTime = LocalTime.now()
+        )
+        val car = Car(
+            zone = zone,
+            name = "벤츠"
+        )
 
-        tx.begin()
-
-        println("==-")
-        // val img = Zip("zip", 10)
-        // em.persist(img)
-        println("==-")
-
-        tx.commit()
-        em.clear()
-
-        tx.begin()
-        // em.find(Car::class.java, 1L)
-
-        val z1 = Zone("asdsa", "asdad", LocalTime.now(), LocalTime.now())
-        val z2 = Zone("asdsa", "asdad", LocalTime.now(), LocalTime.now())
-        val z3 = Zone("asdsa", "asdad", LocalTime.now(), LocalTime.now())
-
-        val c1 = Car(z1, "asd")
-        val c2 = Car(z2, "asd")
-        val c3 = Car(z3, "asd")
-
-        em.persist(z1)
-        em.persist(z2)
-        em.persist(z3)
-
-        em.persist(c1)
-        em.persist(c2)
-        em.persist(c3)
-
-        em.clear()
+        em.persist(zone)
+        em.persist(car)
 
         // n + 1
         em.createQuery("SELECT c FROM Car c JOIN FETCH c.zone", Car::class.java).resultList.forEach {
@@ -62,25 +43,3 @@ class TcJpaStudyApplication(
 fun main(args: Array<String>) {
     runApplication<TcJpaStudyApplication>(*args)
 }
-
-class Mydata(val data1: Int, val data2: Int) {
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Mydata
-
-        if (data1 != other.data1) return false
-        if (data2 != other.data2) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = data1
-        result = 31 * result + data2
-        return result
-    }
-}
-
